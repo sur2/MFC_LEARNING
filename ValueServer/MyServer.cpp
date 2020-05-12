c30e7d6984cc29196bca05671d01966c3a240915
정표용 <sur2@naver.com>
2020-05-12 23:29:55 +0900

    CSocket

---
+++ ValueServer/MyServer.cpp
@@ -0,0 +1,27 @@
+﻿// MyServer.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ValueServer.h"
+#include "MyServer.h"
+
+
+// MyServer
+
+MyServer::MyServer()
+{
+}
+
+MyServer::~MyServer()
+{
+}
+
+
+// MyServer 멤버 함수
+void MyServer::OnAccept(int nErrorCode)
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	Accept(m_user);
+
+	CSocket::OnAccept(nErrorCode);
+}
---
+++ ValueServer/MyUser.cpp
@@ -0,0 +1,44 @@
+﻿// MyUser.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ValueServer.h"
+#include "MyUser.h"
+
+
+// MyUser
+
+MyUser::MyUser()
+{
+}
+
+MyUser::~MyUser()
+{
+}
+
+
+// MyUser 멤버 함수
+void MyUser::OnReceive(int nErrorCode)
+{
+	// int형 값을 받는다고 가정한 경우
+	int value;
+	Receive(&value, sizeof(int));
+	
+	// Main Window 주소값 ->
+	AfxGetMainWnd()->SetDlgItemInt(IDC_VALUE_EDIT, value);
+
+	value = 1;
+	Send(&value, sizeof(int));
+
+	CSocket::OnReceive(nErrorCode);
+}
+
+void MyUser::OnClose(int nErrorCode)
+{
+	CSocket::OnClose(nErrorCode);
+	
+	// 전송 중인 경우에 초기화
+	ShutDown(2); // 2 : 읽기, 쓰기 초기화
+	// 소켓 닫기 
+	Close();
+}
